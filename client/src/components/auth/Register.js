6694356b6b4f2f08fae5b95239e2d907a7f61448
anna li <annadan2005@yahoo.com>
2022-07-03 09:59:52 -0600

    added first last name and image to user

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,13 +2,13 @@ import { useState } from 'react';
 import { AuthConsumer } from '../../providers/AuthProvider';
 
 const Register = ({ handleRegister }) => {
-  const [ user, setUser ] = useState({ email: '', password: '', passwordConfirmation: ''})
+  const [ user, setUser ] = useState({ email: '', password: '', passwordConfirmation: '', first: '', last: '', image: '' })
  
   const handleSubmit = (e) => {
     e.preventDefault()
     if (user.password === user.passwordConfirmation){
       handleRegister(user)
-      setUser({email: '', password: '', passwordConfirmation: ''})
+      setUser({email: '', password: '', passwordConfirmation: '', first: '', last: '', image: ''})
     } else {
        alert('password does not match!')
     }
@@ -27,6 +27,30 @@ const Register = ({ handleRegister }) => {
         required
        />
 
+      <label>First Name</label>
+       <input 
+        name='first'
+        value={user.first}
+        onChange={(e) => setUser({ ...user, first: e.target.value})}
+        required
+       />   
+
+      <label>Last Name</label>
+       <input 
+        name='last'
+        value={user.last}
+        onChange={(e) => setUser({ ...user, last: e.target.value})}
+        required
+       />
+
+      <label>Profile Image</label>
+       <input 
+        name='image'
+        value={user.image}
+        onChange={(e) => setUser({ ...user, image: e.target.value})}
+        required
+       />
+      
        <label>Password</label>
        <input 
         name='password'
